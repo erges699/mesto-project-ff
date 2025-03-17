ddc4ceb5d5c8dcf3f9555fb2bb70f45db3a31362
Serge <erges699@gmail.com>
2025-03-17 22:01:19 +0300

    20250317-01

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/erges699/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
+const container = document.querySelector('.content');
+const cardsPlaces = container.querySelector('.places__list')
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function createCard(titleValue, linkValue ) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = linkValue;
+    cardElement.querySelector('.card__image').textContent = titleValue;
+    cardElement.querySelector('.card__title').textContent = titleValue;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+    cardsPlaces.append(cardElement);
+  }
 
 // @todo: Функция удаления карточки
+function deleteCard() {
+    const cardToDelete = document.querySelector('.places__item');
+    cardToDelete.remove();
+    //console.log(`${cardToDelete}`);
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (card) {
+    createCard(card.name, card.link);
+});
